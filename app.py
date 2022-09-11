4ae8780499ec651c5cf08c5595bcc05833c0e1c2
Sergio Lucero <sergiolucero@gmail.com>
2022-09-11 19:34:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.header('My Streamlit Dashboards')
 
-st.markdown('<A HREF="https://sergiolucero-feriaslibres-app-3aztis.streamlitapp.com/">Ferias Libres</A>', , unsafe_allow_html=True)
+st.markdown('<A HREF="https://sergiolucero-feriaslibres-app-3aztis.streamlitapp.com/">Ferias Libres</A>', unsafe_allow_html=True)
