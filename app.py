56539eedad1b419339c93eb558a3c1ac59ef6550
Sergio Lucero <sergiolucero@gmail.com>
2022-09-11 19:36:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
 st.header('My Streamlit Dashboards')
-
-st.markdown('<A HREF="https://sergiolucero-feriaslibres-app-3aztis.streamlitapp.com/">Ferias Libres</A>', unsafe_allow_html=True)
+hdick = {'Ferias Libres': '<A HREF="https://sergiolucero-feriaslibres-app-3aztis.streamlitapp.com/">%s</A>'}
+for tit, html in hdick.items():
+    st.markdown( html %tit, unsafe_allow_html=True)
